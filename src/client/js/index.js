048a63420d27afe607266c4148ec94803b73d6e0
Fayzulloh <ShodiyevF@yandex.ru>
2023-01-13 19:05:26 +0500

    authorization

--- src/client/js/index.js
+++ src/client/js/index.js
@@ -1,16 +1,20 @@
 import { alert } from "../utils/alert.js"
 
 const form = document.querySelector('.form')
-const input_login = document.querySelector('.input-login')
-const input_password = document.querySelector('.input-password')
+const input_login = document.querySelector('#inputLogin')
+const input_password = document.querySelector('#inputPassword')
 const alert_wrapper = document.querySelector('.alert_wrapper')
 
 form.addEventListener('submit', async (e) => {
     e.preventDefault()
-    if(alert_wrapper.childElementCount <= 2){
-        alert_wrapper.appendChild(alert('asdfasdads~asdasd', 10, 'danger'))
-    }
+    console.log(input_login);
+    input_login.style.borderColor = '#dee2e6'
 
+    if(!input_login.value){
+        input_login.style.borderColor = 'red'
+    } else if(!input_password.value){
+        input_password.style.borderColor = 'red'
+    } else {
         const response = await fetch('/api/login', {
             method: 'POST',
             headers: {
@@ -22,8 +26,15 @@ form.addEventListener('submit', async (e) => {
             })
         })
 
-
-        console.log(response)
+        if(response.redirected == true){
+            window.location = '/'
+        } else {
+            const data = await response.json()
+            if(alert_wrapper.childElementCount <= 2){
+                alert_wrapper.appendChild(alert(data.message, 10, 'danger'))
+            }
+        }
+    }
 })
 
 
